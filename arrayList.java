8424c08f59921421642bdf5e1de6576e96a9bf72
Krishna Agarwal <krishna016agrawal@gmail.com>
2025-06-06 11:04:20 +0530

    commit

--- arrayList.java
+++ arrayList.java
@@ -57,6 +57,36 @@ public class arrayList {
         }
     }
 
+    public static void pair_sum_rotated_array(ArrayList<Integer> list, int target) {
+        int breakpoint = -1;
+        int n = list.size();
+        boolean isFound = false;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) > list.get(i + 1)) {
+                breakpoint = i;
+                break;
+            }
+        }
+        int lp = breakpoint + 1;
+        int rp = breakpoint;
+
+        while (lp != rp) {
+            int sum = list.get(lp) + list.get(rp);
+            if (sum == target) {
+                isFound = true;
+                System.out.println("(" + list.get(lp) + "," + list.get(rp) + ")");
+            }
+            if (sum < target) {
+                lp = (lp + 1) % n;
+            } else {
+                rp = (n + rp - 1) % n;
+            }
+        }
+        if (!isFound) {
+            System.out.println("no pair exist");
+        }
+    }
+
     public static void main(String args[]) {
         // ArrayList<Integer> height = new ArrayList<>();
         // height =[1,8,6,2,5,4,8,3,7]
@@ -72,8 +102,6 @@ public class arrayList {
         // System.out.println(container_with_most_water(height));
         // System.out.println(container_with_most_water_two_pointer_approach(height));
 
-        
-        
         // ArrayList<Integer> number = new ArrayList<>();
         // number.add(1);
         // number.add(2);
@@ -82,5 +110,16 @@ public class arrayList {
         // number.add(5);
         // number.add(6);
         // pair_sum_two_pointer_approach(number, 6);
+
+       
+        // ArrayList<Integer> list = new ArrayList<>();
+        // list.add(11);
+        // list.add(15);
+        // list.add(5);
+        // list.add(6);
+        // list.add(8);
+        // list.add(9);
+        // list.add(10);
+        // pair_sum_rotated_array(list, 16);
     }
 }
