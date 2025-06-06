698c265305c3cdcb9efc560a0e6e80e040889eac
Krishna Agarwal <krishna016agrawal@gmail.com>
2025-06-06 09:22:10 +0530

    commit

--- arrayList.java
+++ arrayList.java
@@ -35,19 +35,52 @@ public class arrayList {
         return max_water;
     }
 
+    public static void pair_sum_two_pointer_approach(ArrayList<Integer> number, int target) {
+        int lp = 0;
+        int rp = number.size() - 1;
+        boolean isFound = false;
+        while (lp < rp) {
+            int sum = number.get(lp) + number.get(rp);
+
+            if (sum == target) {
+                isFound = true;
+                System.out.println("(" + number.get(lp) + "," + number.get(rp) + ")");
+            }
+            if (sum >= target) {
+                rp--;
+            } else {
+                lp++;
+            }
+        }
+        if (!isFound) {
+            System.out.println("no pair exist");
+        }
+    }
+
     public static void main(String args[]) {
-        ArrayList<Integer> height = new ArrayList<>();
+        // ArrayList<Integer> height = new ArrayList<>();
         // height =[1,8,6,2,5,4,8,3,7]
-        height.add(1);
-        height.add(8);
-        height.add(6);
-        height.add(2);
-        height.add(5);
-        height.add(4);
-        height.add(8);
-        height.add(3);
-        height.add(7);
-        System.out.println(container_with_most_water(height));
-        System.out.println(container_with_most_water_two_pointer_approach(height));
+        // height.add(1);
+        // height.add(8);
+        // height.add(6);
+        // height.add(2);
+        // height.add(5);
+        // height.add(4);
+        // height.add(8);
+        // height.add(3);
+        // height.add(7);
+        // System.out.println(container_with_most_water(height));
+        // System.out.println(container_with_most_water_two_pointer_approach(height));
+
+        
+        
+        // ArrayList<Integer> number = new ArrayList<>();
+        // number.add(1);
+        // number.add(2);
+        // number.add(3);
+        // number.add(4);
+        // number.add(5);
+        // number.add(6);
+        // pair_sum_two_pointer_approach(number, 6);
     }
 }
