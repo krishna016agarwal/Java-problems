6df09a4f9ab974b20ca850071245d08938e10fa8
Krishna Agarwal <krishna016agrawal@gmail.com>
2025-06-14 16:45:19 +0530

    commit

--- pattern.java
+++ pattern.java
@@ -34,9 +34,46 @@ public class pattern {
     }
   }
 
+  public static void diamond(int n) {
+    //top
+    for (int i = 0; i < n; i++) {
+      // spaces
+      for (int j = 0; j < n-i; j++) {
+        System.out.print(" ");
+      }
+      // stars
+      for (int k = 0; k < 2 * i + 1; k++) {
+        System.out.print("*");
+      }
+      // spaces
+      for (int l = 0; l <n-i; l++) {
+        System.out.print(" ");
+      }
+      System.out.print("\n");
+    }
+//bottom
+
+for (int i = n; i >0; i--) {
+      // spaces
+      for (int j = n-i; j >=0; j--) {
+        System.out.print(" ");
+      }
+      // stars
+      for (int k = 2 * (i-1) + 1; k >0 ; k--) {
+        System.out.print("*");
+      }
+      // spaces
+      for (int j = n-i; j >=0; j--) {
+        System.out.print(" ");
+      }
+      System.out.print("\n");
+    }
+  }
+
   public static void main(String[] args) {
 
     // draw_number_rectangle(3);
     // triangle_of_number(5);
+   // diamond(5);
   }
 }
