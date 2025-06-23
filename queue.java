f56aefb2442ce500d0c3e6722d18c90ab86ca9ef
Krishna Agarwal <krishna016agrawal@gmail.com>
2025-06-23 14:56:23 +0530

    commit

--- queue.java
+++ queue.java
@@ -168,7 +168,137 @@ public class queue {
         }
     }
 
-  
+    static class make_Queue_using_two_stack {
+        static Stack<Integer> s1 = new Stack<>();
+        static Stack<Integer> s2 = new Stack<>();
+
+        public static boolean isEmpty() {
+            return s1.isEmpty();
+        }
+
+        public static void add(int data) { // O(n)
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+            s1.push(data);
+            while (!s2.isEmpty()) {
+                s1.push(s2.pop());
+            }
+        }
+
+        public static int remove() { // O(1)
+            if (s1.isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+            return s1.pop();
+        }
+
+        public static int peek() {
+            if (s1.isEmpty()) {
+                System.out.println("empty queue");
+                return -1;
+            }
+            return s1.peek();
+        }
+    }
+
+    static class make_stack_using_two_queue {
+        static Queue<Integer> q1 = new LinkedList<>();
+        static Queue<Integer> q2 = new LinkedList<>();
+
+        public static boolean isEmpty() {
+            return q1.isEmpty() && q2.isEmpty();
+        }
+
+        public static void add(int data) { // O(1)
+            if (!q1.isEmpty()) {
+                q1.add(data);
+            } else {
+                q2.add(data);
+            }
+
+        }
+
+        public static int pop() { // O(n)
+            if (isEmpty()) {
+                System.out.println("empty stack");
+                return -1;
+            }
+            int top = -1;
+
+            // case 1
+            if (!q1.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+                    if (q1.isEmpty()) {
+                        break;
+                    }
+                    q2.add(top);
+                }
+
+            } else { // case 2
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+                    if (q2.isEmpty()) {
+                        break;
+                    }
+                    q1.add(top);
+                }
+
+            }
+            return top;
+        }
+
+        public static int peek() { // O(n)
+            if (isEmpty()) {
+                System.out.println("empty stack");
+                return -1;
+            }
+            int top = -1;
+
+            // case 1
+            if (!q1.isEmpty()) {
+                while (!q1.isEmpty()) {
+                    top = q1.remove();
+
+                    q2.add(top);
+                }
+
+            } else { // case 2
+                while (!q2.isEmpty()) {
+                    top = q2.remove();
+
+                    q1.add(top);
+                }
+
+            }
+            return top;
+        }
+    }
+
+    public static void non_repeating_letters_in_a_stream_of_characters(String str) {
+        String str2 = "";
+        int fre[] = new int[26]; // a-z
+        Queue<Character> q = new LinkedList<>();
+        for (int i = 0; i < str.length(); i++) {
+            char a = str.charAt(i);
+            fre[a - 'a']++; // 'a' - 'a' → 0 / 'b'- 'a' → 1 / 'c'- 'a' → 25
+           
+            q.add(a);
+            str2 = str2 + a;
+            while (condition) {
+                
+            }
+             if (fre[q.peek()-'a']==1) {
+                System.out.println(q.peek());
+            }else{
+                q.remove();
+                System.out.println("-1");
+            }
+        }
+    }
+
     public static void main(String args[]) {
         // ArrayQueue s=new ArrayQueue(5) //non static method
 
@@ -195,7 +325,7 @@ public class queue {
         // System.out.println(RotatedArrayQueue.peek());
         // RotatedArrayQueue.remove();
         // }
-        
+
         // ------------------------------------------------------------------------
 
         // new LinkedListQueue();
@@ -212,25 +342,46 @@ public class queue {
 
         // ------------------------------------------------
 
-        // Queue<Integer> q=new LinkedList<>(); 
+        // Queue<Integer> q=new LinkedList<>();
         // q.add(1);//O(n)
         // q.add(2);
         // q.add(3);
         // while (!q.isEmpty()) {
-        //     System.out.println(q.peek()); //O(n)
-        //     q.remove();//O(n)
+        // System.out.println(q.peek()); //O(n)
+        // q.remove();//O(n)
         // }
-
-        //  Queue<Integer> s=new ArrayDeque<>(); //ArrayDeque
+        // -----------------------------------------------------------------
+        // Queue<Integer> s=new ArrayDeque<>(); //ArrayDeque
         // s.add(1);
         // s.add(2);//O(n)
         // s.add(3);
         // while (!s.isEmpty()) {
-        //     System.out.println(s.peek());//O(n)
-        //     s.remove();//O(n)
+        // System.out.println(s.peek());//O(n)
+        // s.remove();//O(n)
         // }
 
-    
+        // ---------------make_Queue_using_two_stack------------------------
+
+        // new make_Queue_using_two_stack();
+        // make_Queue_using_two_stack.add(1);
+        // make_Queue_using_two_stack.add(2);
+        // make_Queue_using_two_stack.add(3);
+        // make_Queue_using_two_stack.add(4);
+        // while (!make_Queue_using_two_stack.isEmpty()) {
+        // System.out.println(make_Queue_using_two_stack.peek());
+        // make_Queue_using_two_stack.remove();
+        // }
+
+        // -------------------make_stack_using_two_queue------------------------------------
+
+        // new make_stack_using_two_queue();
+        // make_stack_using_two_queue.add(1);
+        // make_stack_using_two_queue.add(2);
+        // make_stack_using_two_queue.add(3);
+        // while (!make_stack_using_two_queue.isEmpty()) {
+        // System.out.println(make_stack_using_two_queue.peek());
+        // make_stack_using_two_queue.pop();
+        // }
 
     }
 }
