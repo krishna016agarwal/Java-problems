15b089ea37f54a951f798e4fe40a998ddb8bb106
Krishna Agarwal <krishna016agrawal@gmail.com>
2025-06-26 13:24:24 +0530

    commit

--- Greedy.java
+++ Greedy.java
@@ -89,6 +89,88 @@ public class Greedy {
         System.out.println(finalval);
     }
 
+    public static void min_sum_absolute_difference_pairs(int a[], int b[]) {
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int sum = 0;
+        for (int i = 0; i < b.length; i++) {
+            sum += Math.abs(a[i] - b[i]);
+        }
+        System.out.println(sum);
+    }
+
+    public static void maximum_length_chain_of_pair(int pair[][]) { // O(nlogn)
+        Arrays.sort(pair, Comparator.comparing(o -> o[1]));// sort 2d array //sorting on the basis of column 2 index 1
+        int chainLength = 1;
+
+        int chainEnd = pair[0][1];
+        for (int i = 0; i < pair.length; i++) {
+            if (pair[i][0] > chainEnd) {
+                chainLength++;
+                chainEnd = pair[i][1];
+
+            }
+        }
+        System.out.println("max length of chain " + chainLength);
+
+    }
+
+    public static void make_amount_with_quantised_indian_Coins(int target, Integer coin[]) {
+        Arrays.sort(coin, Comparator.reverseOrder());
+        ArrayList<Integer> s = new ArrayList<>();
+        int minCoinsUsed = 0;
+        for (int i = 0; i < coin.length; i++) {
+            if (target >= coin[i]) {
+                while (target >= coin[i]) {
+                    minCoinsUsed++;
+                    target -= coin[i];
+                    s.add(coin[i]);
+                }
+            }
+        }
+        System.out.println("min coins used " + minCoinsUsed);
+        for (int i = 0; i < s.size(); i++) {
+
+            System.out.print(s.get(i) + " ");
+        }
+
+    }
+
+    public static class job {
+        public static class Job {
+
+            int id;
+            int deadline;
+            int profit;
+
+            Job(int i, int d, int p) {
+                id = i;
+                deadline = d;
+                profit = p;
+            }
+        }
+
+        public static void job_maximum_profit(ArrayList<Job> s) {
+            Collections.sort(s, (obj1, obj2) -> obj2.profit - obj1.profit); // descending order
+            ArrayList<Integer> q = new ArrayList<>();
+            int time = 0;
+            for (int i = 0; i < s.size(); i++) {
+                Job curr = s.get(i);
+                if (curr.deadline > time) {
+                    q.add(curr.id);
+                    time++;
+                }
+
+            }
+
+            // print
+            System.out.println("max jobs " + q.size());
+            for (int i = 0; i < q.size(); i++) {
+                System.out.print(q.get(i) + " ");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // int start[] = { 1, 3, 0, 5, 8, 5 };
         // int end[] = { 2, 4, 6, 7, 9, 9 };
@@ -101,6 +183,30 @@ public class Greedy {
         // int weight[] = { 10, 20, 30, 10 };
         // int maxWeight = 50;
         // fractionalKnapsack(value, weight, maxWeight);
+
+        // ------------------------------------
+        // int a[]={4,1,8,7};
+        // int b[]={2,3,6,5};
+        // min_sum_absolute_difference_pairs(a, b);
+
+        // ----------------------------
+        // int pair[][]={{5,24},{39,60},{5,28},{27,40},{50,90}};
+        // maximum_length_chain_of_pair(pair);
+
+        // -----------------------------------
+
+        // Integer coin[]={1,2,5,10,20,50,100,500,2000};
+        // make_amount_with_quantised_indian_Coins(1059, coin);
+
+        // -----------------------------------------
+        
+        // int jobInfo[][] = { { 4, 20 }, { 1, 10 }, { 1, 40 }, { 1, 30 } };
+        // ArrayList<job.Job> s = new ArrayList<>();
+
+        // for (int i = 0; i < jobInfo.length; i++) {
+        //     s.add(new job.Job(i, jobInfo[i][0], jobInfo[i][1]));
+        // }
+        // job.job_maximum_profit(s);
 
     }
 }
