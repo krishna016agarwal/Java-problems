8f330ce0c4ba5596515a351ff154b4e678f6dd98
Krishna Agarwal <krishna016agrawal@gmail.com>
2025-06-30 08:45:37 +0530

    commit

--- binaryTree.java
+++ binaryTree.java
@@ -304,6 +304,7 @@ public class binaryTree {
         int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
 
         BinaryTree tree = new BinaryTree();
+        
 
         Node root = tree.buildTree(nodes);
 
