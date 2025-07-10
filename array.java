ff2c25354734558daaf17731700628d98aa34ad5
Krishna Agarwal <krishna016agrawal@gmail.com>
2025-07-10 09:31:24 +0530

    commit

--- array.java
+++ array.java
@@ -20,86 +20,104 @@ public class array {
     public static int remove_duplicate_in_sorted_array(int arr[]) {
         // Deque<Integer> q = new LinkedList<>();
         // for (int i = 0; i < arr.length; i++) {
-            
-        //     if (q.peek()!=null && q.peekLast() != arr[i]) {
-        //         q.add(arr[i]);
-        //     }else if(q.peek()==null){
-        //         q.add(arr[i]);
-        //     }
+
+        // if (q.peek()!=null && q.peekLast() != arr[i]) {
+        // q.add(arr[i]);
+        // }else if(q.peek()==null){
+        // q.add(arr[i]);
+        // }
 
         // }
         // int size=q.size();
         // for (int i = 0; i < arr.length; i++) {
-        //     if (!q.isEmpty()) {
-        //         arr[i] = q.remove();
-        //     } else {
-        //         arr[i] = 0;
-        //     }
+        // if (!q.isEmpty()) {
+        // arr[i] = q.remove();
+        // } else {
+        // arr[i] = 0;
+        // }
 
         // }
         // for (int i = 0; i < size; i++) {
-        //     System.out.print(arr[i]+" ");
+        // System.out.print(arr[i]+" ");
         // }
         // return size;
 
-        //------------------------------------------
-        
-        int j=0;
-        if(arr.length==0) return 0;
-        if(arr.length==1) return 1;
+        // ------------------------------------------
+
+        int j = 0;
+        if (arr.length == 0)
+            return 0;
+        if (arr.length == 1)
+            return 1;
         for (int i = 1; i < arr.length; i++) {
-            if (arr[j]!=arr[i]) {
+            if (arr[j] != arr[i]) {
                 j++;
-                arr[j]=arr[i];
+                arr[j] = arr[i];
             }
         }
-        return j+1;
+        return j + 1;
     }
 
-   public static void rotateArray(int arr[],int k){
- 
-   if (arr.length==0 || arr.length==1 ||k==0) {
-    return;
-   }
- 
-   if (arr.length%2==0 && k%2==0) {
-    int i=0;
-    while (i<k) {
-        int temp=arr[i];
-        arr[i]=arr[(i+k)%arr.length];
-        arr[(i+k)%arr.length]=temp;
-        i++;
+    public static void reverseArray(int arr[],int n,int m){
+        while(n<m){
+            arr[n]=arr[n]+arr[m];
+        arr[m]=arr[n]-arr[m];
+        arr[n]=arr[n]-arr[m];
+        n++;
+        m--;
+        }
     }
     
-    return;
-   }
-     int temp=arr[0];
-    int next=k%arr.length;
-     while (temp!=arr[next]) {
-      
-        temp=(temp)+(arr[next]);
-    arr[next]=(temp)-(arr[next]);
-    temp=(temp)-(arr[next]);
-    next=(next+3)%arr.length;
+    public static void rotateArray(int arr[], int k) {
 
-     }
- 
-   
+        // if (arr.length == 0 || arr.length == 1 || k == 0) {
+        //     return;
+        // }
+        // int n = arr.length;
+        // k = k % n;
 
-   }
-   
+        // int j = 0;
+        // for (int i = n - k; i <= (n - 1 + n - k) / 2; i++) {
+        //     int temp = arr[i];
+        //     arr[i] = arr[n - (++j)];
+        //     arr[n - (j)] = temp;
+        // }
+        
+        // for (int i = 0; i <= (n - k - 1) / 2; i++) {
+        //     int temp = arr[i];
+        //     arr[i] = arr[n - k - 1 - i];
+        //     arr[n - k - 1 - i] = temp;
+        // }
+        
+     
+        //     for (int i = 0; i < n / 2 ; i++) {
+        //         int temp = arr[i];
+        //         arr[i] = arr[n - i-1];
+        //         arr[n - i-1] = temp;
+        //     }
+
+        //----------------------------------------------------------
+        int n=arr.length;
+        k=k%n;
+       reverseArray(arr, n-k, n-1);
+       reverseArray(arr, 0, n-k-1);
+       reverseArray(arr, 0, n-1);
+
+    }
+
     public static void main(String[] args) {
         // int arr[] = { 1,1,2,2,2, 2, 7, 7 };
         // System.out.println(secondLargest(arr));
 
-        //  int arr[] = { 1,1,2,2,2, 2, 7, 7,8,8,9,10,10 };
-       // remove_duplicate_in_sorted_array(arr);
+        // int arr[] = { 1,1,2,2,2, 2, 7, 7,8,8,9,10,10 };
+        // remove_duplicate_in_sorted_array(arr);
         // System.out.println(remove_duplicate_in_sorted_array(arr));
-        
-        int arr[]={-1,-100,3,99};
-        rotateArray(arr,2);
-        for (int i = 0; i < arr.length; i++) {
-    System.out.print(arr[i]+" ");
-   }
+
+    //     int arr[] = { 1,2,3,4 };
+    //     rotateArray(arr, 6);
+    //  //  reverseArray(arr, 0, arr.length-1);
+    //     for (int i = 0; i < arr.length; i++) {
+    //         System.out.print(arr[i] + " ");
+    //     }
     }
 }
