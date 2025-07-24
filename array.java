25bc3fc2ad5a532f699212aee3cfc2499009bce5
Krishna Agarwal <krishna016agrawal@gmail.com>
2025-07-24 16:03:37 +0530

    spiralOrder

--- array.java
+++ array.java
@@ -749,7 +749,7 @@ public class array {
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = i; j < matrix[0].length; j++) {
-               swap(matrix[i], i, j);
+                swap(matrix[i], i, j);
             }
         }
 
@@ -757,6 +757,36 @@ public class array {
             reverseArray(matrix[i], 0, matrix[i].length - 1);
         }
         print_2d_array(matrix);
+    }
+
+    public static List<Integer> spiralOrder(int[][] matrix) { //O(m*n) SC- O(m*n)
+        ArrayList<Integer> s = new ArrayList<>();
+        int top = 0, left = 0, right = matrix[0].length - 1, bottom = matrix.length - 1;
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) { // L -> R
+                s.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) { // T -> B
+                s.add(matrix[i][right]);
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) { // R -> L
+                    s.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {// B -> T
+                    s.add(matrix[i][left]);
+                }
+                left++;
+            }
+
+        }
+
+        return s;
     }
 
     public static void main(String[] args) {
@@ -852,7 +882,11 @@ public class array {
         // subarray_sum_k_brute_case(arr, 33);
         // total_subattay_with_sum_k_and_longest_subarry(arr, 33);
 
-        int arr[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
-        rotate_array_by_90_degree(arr);
+        // int arr[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14,
+        // 15, 16 } };
+        // rotate_array_by_90_degree(arr);
+
+        int arr[][] = { { 1, 2, 3 }, { 5, 6, 7 }, { 9, 10, 11 } };
+        System.out.println(spiralOrder(arr));
     }
 }
