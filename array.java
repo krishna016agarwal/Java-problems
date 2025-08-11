e7e4851f95cedd4ea31831e98a75ee75d22763b4
Krishna Agarwal <krishna016agrawal@gmail.com>
2025-08-11 21:28:59 +0530

    searchinsert

--- array.java
+++ array.java
@@ -1244,6 +1244,26 @@ public class array {
         return max;
     }
 
+    public static int searchInsert(int[] nums, int target) {
+        int si=0;
+        int ei=nums.length-1;
+       
+        while(si<=ei){
+            int mid=si+(ei-ei)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) {
+                ei=mid-1;
+              
+            }
+            else {si=mid+1;
+        
+            }
+          
+        }
+        return si;
+    
+    }
+
     public static void main(String[] args) {
 
         // int arr[] = { 1,1,2,2,2, 2, 7, 7 };
@@ -1398,6 +1418,9 @@ public class array {
 
         // int arr[]={2,0,-1,3};
         // System.out.println(maxProduct(arr));
+
+        // int arr[]={1,2,3,4,5,6};
+        // System.out.println(searchInsert(arr, 7));
 
     }
 }
