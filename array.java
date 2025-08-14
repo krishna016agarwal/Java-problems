a544266ee732a312a30085f33f843855d52a7cd5
Krishna Agarwal <krishna016agrawal@gmail.com>
2025-08-14 12:46:45 +0530

    search_in_rotated_array

--- array.java
+++ array.java
@@ -1244,7 +1244,7 @@ public class array {
         return max;
     }
 
-    public static int searchInsert(int[] nums, int target) {
+    public static int searchInsert(int[] nums, int target) { //O(logn)
         int si = 0;
         int ei = nums.length - 1;
 
@@ -1265,7 +1265,7 @@ public class array {
 
     }
 
-    public static int findFloor(int[] arr, int x) {
+    public static int findFloor(int[] arr, int x) { //O(logn)
         int ans = -1;
         int si = 0;
         int ei = arr.length - 1;
@@ -1281,7 +1281,7 @@ public class array {
         return ans;
     }
 
-    public static int[] search_first_and_last_index_of_element(int[] nums, int target) {
+    public static int[] search_first_and_last_index_of_element(int[] nums, int target) { //O(logn)
         int si = 0;
         int ei = nums.length - 1;
         int arr[] = { -1, -1 };
@@ -1311,7 +1311,7 @@ public class array {
         return arr;
     }
 
-    public static int countFreq_in_sorted_array(int[] arr, int target) {
+    public static int countFreq_in_sorted_array(int[] arr, int target) { //O(logn)
         int si = 0;
         int ei = arr.length - 1;
         int firstindex = -1;
@@ -1346,6 +1346,30 @@ public class array {
 
     }
 
+    public static int search_in_rotated_array(int[] nums, int target) { //)(logn)
+        int si=0;
+        int ei=nums.length-1;
+        while(si<=ei){
+            int mid=si+(ei-si)/2;
+            if(nums[mid]==target)return mid;
+            if(nums[si]<=nums[mid]){
+                if(nums[si]<=target && target<=nums[mid]){
+                    ei=mid-1;
+                }else{
+                    si=mid+1;
+                }
+            }else{
+                 if(nums[mid]<=target && target<=nums[ei]){
+                       si=mid+1;
+                }else{
+                 
+                    ei=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+   
     public static void main(String[] args) {
 
         // int arr[] = { 1,1,2,2,2, 2, 7, 7 };
@@ -1511,8 +1535,11 @@ public class array {
         // int nums[]=search_first_and_last_index_of_element(arr, 8);
         // printArray(nums);
 
-        int arr[] = { 1, 1, 2, 2, 2, 2, 3 };
-        System.out.println(countFreq_in_sorted_array(arr, 2));
+        // int arr[] = { 1, 1, 2, 2, 2, 2, 3 };
+        // System.out.println(countFreq_in_sorted_array(arr, 2));
+
+        // int arr[]={7,8,9,1,2,3,4};
+        // System.out.println(search_in_rotated_array(arr,3));
 
     }
 }
