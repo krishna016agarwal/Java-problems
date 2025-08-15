1044b54ff19ff581fb519d6de7fc4d43cf7232f9
Krishna Agarwal <krishna016agrawal@gmail.com>
2025-08-15 18:09:45 +0530

    search_in_rotated_array_with_dulpicates

--- array.java
+++ array.java
@@ -1244,7 +1244,7 @@ public class array {
         return max;
     }
 
-    public static int searchInsert(int[] nums, int target) { //O(logn)
+    public static int searchInsert(int[] nums, int target) { // O(logn)
         int si = 0;
         int ei = nums.length - 1;
 
@@ -1265,7 +1265,7 @@ public class array {
 
     }
 
-    public static int findFloor(int[] arr, int x) { //O(logn)
+    public static int findFloor(int[] arr, int x) { // O(logn)
         int ans = -1;
         int si = 0;
         int ei = arr.length - 1;
@@ -1281,7 +1281,7 @@ public class array {
         return ans;
     }
 
-    public static int[] search_first_and_last_index_of_element(int[] nums, int target) { //O(logn)
+    public static int[] search_first_and_last_index_of_element(int[] nums, int target) { // O(logn)
         int si = 0;
         int ei = nums.length - 1;
         int arr[] = { -1, -1 };
@@ -1311,7 +1311,7 @@ public class array {
         return arr;
     }
 
-    public static int countFreq_in_sorted_array(int[] arr, int target) { //O(logn)
+    public static int countFreq_in_sorted_array(int[] arr, int target) { // O(logn)
         int si = 0;
         int ei = arr.length - 1;
         int firstindex = -1;
@@ -1339,37 +1339,99 @@ public class array {
             else
                 si = mid + 1;
         }
-        if (lastindex==-1&&firstindex==-1) {
+        if (lastindex == -1 && firstindex == -1) {
             return 0;
         }
         return lastindex - firstindex + 1;
 
     }
 
-    public static int search_in_rotated_array(int[] nums, int target) { //)(logn)
-        int si=0;
-        int ei=nums.length-1;
-        while(si<=ei){
-            int mid=si+(ei-si)/2;
-            if(nums[mid]==target)return mid;
-            if(nums[si]<=nums[mid]){
-                if(nums[si]<=target && target<=nums[mid]){
-                    ei=mid-1;
-                }else{
-                    si=mid+1;
+    public static int search_in_rotated_array(int[] nums, int target) { // )(logn)
+        int si = 0;
+        int ei = nums.length - 1;
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[si] <= nums[mid]) {
+                if (nums[si] <= target && target <= nums[mid]) {
+                    ei = mid - 1;
+                } else {
+                    si = mid + 1;
                 }
-            }else{
-                 if(nums[mid]<=target && target<=nums[ei]){
-                       si=mid+1;
-                }else{
-                 
-                    ei=mid-1;
+            } else {
+                if (nums[mid] <= target && target <= nums[ei]) {
+                    si = mid + 1;
+                } else {
+
+                    ei = mid - 1;
                 }
             }
         }
         return -1;
     }
-   
+
+    public static ArrayList<Integer> calculateSpan(int[] arr) {
+        ArrayList<Integer> span = new ArrayList<>();
+        Stack<Integer> s = new Stack<>(); // stores indices
+
+        // Process each day
+        for (int i = 0; i < arr.length; i++) {
+            // Pop elements smaller or equal to current price
+            while (!s.isEmpty() && arr[s.peek()] <= arr[i]) {
+                s.pop();
+            }
+
+            // If stack empty, span is i+1 (all previous are smaller)
+            if (s.isEmpty()) {
+                span.add(i + 1);
+            } else {
+                // Span is distance from nearest greater element to left
+                span.add(i - s.peek());
+            }
+
+            // Push current day index
+            s.push(i);
+        }
+        return span;
+    }
+
+    public static boolean search_in_rotated_array_with_dulpicates(int[] nums, int target) {
+        int si = 0, ei = nums.length - 1;
+
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+
+            if (nums[mid] == target)
+                return true;
+
+            // Skip duplicates
+            if (nums[si] == nums[mid] && nums[mid] == nums[ei]) {
+                si++;
+                ei--;
+                continue;
+            }
+
+            // Left half is sorted
+            if (nums[si] <= nums[mid]) {
+                if (nums[si] <= target && target < nums[mid]) {
+                    ei = mid - 1;
+                } else {
+                    si = mid + 1;
+                }
+            }
+            // Right half is sorted
+            else {
+                if (nums[mid] < target && target <= nums[ei]) {
+                    si = mid + 1;
+                } else {
+                    ei = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
 
         // int arr[] = { 1,1,2,2,2, 2, 7, 7 };
@@ -1540,6 +1602,9 @@ public class array {
 
         // int arr[]={7,8,9,1,2,3,4};
         // System.out.println(search_in_rotated_array(arr,3));
+
+        // int arr[]={3,3,1,2,3,3,3};
+        // System.out.println(search_in_rotated_array_with_dulpicates(arr, 1));
 
     }
 }
