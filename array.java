d43b902467a8c44258048c5f63fb3b36730baa55
Krishna Agarwal <krishna016agrawal@gmail.com>
2025-08-16 18:10:01 +0530

    findKRotation

--- array.java
+++ array.java
@@ -1454,6 +1454,40 @@ public class array {
         return ans;
     }
 
+    
+    public static int findKRotation(int nums[]) {
+       int si = 0;
+        int ei = nums.length - 1;
+        int index=-1;
+        int ans = Integer.MAX_VALUE;
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+
+            if (nums[si] <= nums[ei]) {
+                if(nums[si]<ans){
+                    ans=nums[si];
+                    index=si;
+                }
+                break;
+            } else if (nums[si] <= nums[mid]) {
+                if(nums[si]<ans){
+                    ans=nums[si];
+                    index=si;
+                }
+                si = mid + 1;
+            } else {
+                if(nums[mid]<ans){
+                    ans=nums[mid];
+                    index=mid;
+                }
+                ei = mid - 1;
+            }
+
+        }
+        return index;
+    }
+
+    
     public static void main(String[] args) {
 
         // int arr[] = { 1,1,2,2,2, 2, 7, 7 };
@@ -1630,6 +1664,9 @@ public class array {
 
         // int arr[]={4,5,6,1,2,3};
         // System.out.println(find_minimum_in_rotated_sorted_array(arr));
+
+        //  int arr[]={5,6,1,2,3,4};
+        //  System.out.println(findKRotation(arr));
 
     }
 }
