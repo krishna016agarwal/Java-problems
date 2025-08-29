b594dc98e08265f4af905bfe9ebb2cfdbfa3cd7f
Krishna Agarwal <krishna016agrawal@gmail.com>
2025-08-29 11:26:18 +0530

    findMedianSortedArrays

--- binarySearch.java
+++ binarySearch.java
@@ -657,26 +657,42 @@ public class binarySearch {
     }
 
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int arr[] = new int[nums1.length + nums2.length];
-        int i = 0, j = 0, k = 0;
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] <= nums2[j]) {
-                arr[k++] = nums1[i++];
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if (n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
+        int n = n1 + n2;
+        int left = (n + 1) / 2;
+
+        int low = 0, high = n1;
+        while (low <= high) {
+            int mid1 = low + (high - low) / 2;
+            int mid2 = left - mid1;
+            int l1 = Integer.MIN_VALUE;
+            int l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE;
+            int r2 = Integer.MAX_VALUE;
+            if (mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
+            if (mid1 < n1)
+                r1 = nums1[mid1];
+            if (mid2 < n2)
+                r2 = nums2[mid2];
+            if (l1 <= r2 && l2 <= r1) {
+                if (n % 2 == 0) {
+                    return (double) ((Math.max(l1, l2) + Math.min(r1, r2)) / 2.0);
+                } else {
+                    return Math.max(l1, l2);
+                }
+            } else if (l1 > r2) {
+                high = mid1 - 1;
             } else {
-                arr[k++] = nums2[j++];
+                low = mid1 + 1;
             }
         }
-        while (i < nums1.length) {
-            arr[k++] = nums1[i++];
-        }
-        while (j < nums2.length) {
-            arr[k++] = nums2[j++];
-        }
-        int h = nums1.length + nums2.length;
-        if ((h) % 2 != 0) {
-            return arr[h / 2];
-        }
-        return (arr[h / 2] + arr[(h / 2) - 1]) / 2.0;
+        return 0;
     }
 
     public static void main(String args[]) {
@@ -747,5 +763,7 @@ public class binarySearch {
         // int arr1[]={1,2};
         // int arr2[]={3,4};
         // System.out.println(findMedianSortedArrays(arr1, arr2));
+
+
     }
 }
