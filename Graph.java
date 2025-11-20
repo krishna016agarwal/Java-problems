4470d2338e5e4be6814ffa8cb6988e4fc6c12bd2
Krishna Agarwal <krishna016agrawal@gmail.com>
2025-11-20 21:48:31 +0530

    dfs

--- Graph.java
+++ Graph.java
@@ -28,6 +28,30 @@ public class Graph {
         return ans;
     }
 
+    class DFS {
+        public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+            // code here
+            ArrayList<Integer> a = new ArrayList<>();
+            int arr[] = new int[adj.size()];
+            helper(adj, a, arr, 0);
+            return a;
+
+        }
+
+        public void helper(ArrayList<ArrayList<Integer>> adj, ArrayList<Integer> a, int arr[], int i) {
+            if (arr[i] == 0) {
+                a.add(i);
+                arr[i] = 1;
+                for (int j : adj.get(i)) {
+                    if (arr[j] != 1) {
+                        helper(adj, a, arr, j);
+                    }
+                }
+
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
     }
